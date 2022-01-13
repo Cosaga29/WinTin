68a46e9c3f92b55a2379c7377b58dd989c279852
Martin Edmunds <maedmun2@gmail.com>
2022-01-12 22:42:23 -0500

    cleanup

--- src/shades/shades.py
+++ src/shades/shades.py
@@ -5,6 +5,8 @@ def main():
     cur_node = int(sys.argv[1])
     direction = sys.argv[2]
 
+    # Check if the current node is in the list of connections and the direction
+    # provided by the player is valid
     if cur_node in connections and direction in directions[cur_node]:
         # Get the possible exits for this node
         direction = directions[cur_node][direction]
