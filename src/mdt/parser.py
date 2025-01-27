dc91f5b33f9b328c0fb0ddb07924b74a7b564ba2
Alex <maedmun2@gmail.com>
2025-01-27 18:47:33 -0500

    refactor + cleanup!

---
+++ src/mdt/parser.py
@@ -0,0 +1,243 @@
+import re
+from enum import Enum
+
+from definitions import (
+    RoomInfo,
+    EntityInfo,
+    NUMBER_MAP,
+    DIRECTION_MAP,
+    IGNORE_TOKENS,
+)
+from patterns import PLAYER_COLOR
+from config import MdtColors
+
+
+def filter_exits(lines: list[str]) -> list[str]:
+    num_lines = len(lines)
+    to_return = []
+    current_idx = 0
+    while current_idx < num_lines:
+        line = lines[current_idx]
+        if line.startswith(IGNORE_TOKENS):
+            follow_on_direction = True
+
+            # If line starts with any of these, remove it and any follow on directions...
+            while follow_on_direction and current_idx + 1 < num_lines:
+                next_line = lines[current_idx + 1]
+                words = next_line.split(" ")
+                if len(words) == 1 and words[0] in DIRECTION_MAP:
+                    current_idx += 1
+                elif (
+                    len(words) >= 2
+                    and words[0] in NUMBER_MAP
+                    and words[1] in DIRECTION_MAP
+                ):
+                    current_idx += 1
+                else:
+                    follow_on_direction = False
+        else:
+            to_return.append(line)
+
+        current_idx += 1
+
+    return to_return
+
+
+def push_directions(dir_token_string: str, directions: list[tuple[int, str]]) -> bool:
+    is_last_dir = False
+
+    # Handles the 1 west, 1 south and 1 east case
+    # Note that 'and' is not a descriptor for direction, so this is safe to do unlike for entities
+    if "and" in dir_token_string:
+        is_last_dir = True
+
+    dir_token_string = dir_token_string.replace(" and ", ", ")
+    room_directions = dir_token_string.split(", ")
+
+    for dir in room_directions:
+        space = dir.find(" ")
+        dir_count_token = dir[:space]
+        dir_token = dir[space + 1 :]
+
+        if dir_count_token in NUMBER_MAP:
+            dir_number = NUMBER_MAP[dir_count_token]
+
+        if dir_token in DIRECTION_MAP:
+            dir_token = DIRECTION_MAP[dir_token]
+
+        # The room where these NPCs are
+        directions.append((dir_number, dir_token))
+
+    return is_last_dir
+
+
+def push_entities(entity_token_string: str, entity_stack: list[EntityInfo]):
+    """Method that adds the entity token string to the MDT room data using the entities that
+    have not been associated.
+
+    Args:
+        entity_token_string (str): The entity string
+        entity_stack (list[EntityInfo]): The list of entities that we've seen so far
+    """
+    # an adorable slave, a grey and blue rat and a grinning young man and two strong men and a blue and purple man
+
+    # Separate entities by 'and'
+    entity_token_string = entity_token_string.split(" and ")
+
+    # General method is to parse through the entity string until we encounter a count
+    # each entity is associated with a count, unless it's a named
+    for entity in entity_token_string:
+        e = EntityInfo()
+        e.count = 1
+
+        # See if this entity has special codes associated with it
+        if entity.startswith("\x1b"):
+            # Check that the code matches a player coloring code
+            if s := re.search(PLAYER_COLOR, entity):
+                if len(s.groups()) > 0:
+                    e.count = 1
+                    e.description = s.groups()[0]
+                    e.curse_color_code = MdtColors.GREEN.value
+        else:
+            for word in entity.split(" "):
+                if word in NUMBER_MAP:
+                    e.count = NUMBER_MAP[word]
+                    e.description += f"{word} "
+                else:
+                    e.description += f"{word} "
+
+            e.description = e.description.rstrip()
+        entity_stack.append(e)
+
+
+class MdtContextParser:
+    class State(Enum):
+        DONE = 1
+        ADD_ENTITIES = 2
+        ADD_DIRECTIONS = 3
+        EVAL_TOKEN = 4
+        ADD_ENTITIES_AND_DIRECTIONS = 5
+        GET_TOKEN = 6
+
+    def __init__(self, line: str):
+        self.tokens = self._precondition_lines(line)
+
+        # Precompute this now for reuse
+        self.word_tokens = list(map(lambda x: x.split(" "), self.tokens))
+
+        self.num_tokens = len(self.tokens)
+        self.token_idx = 0
+        self.token: str = ""
+
+        self.state = MdtContextParser.State.DONE
+        self.mdt_rooms: dict[tuple[tuple[int, str]], RoomInfo] = {}
+
+        self.entity_stack = []
+        self.direction_stack = []
+
+        self.entities = ""
+        self.directions = ""
+
+        self.state_executor = {
+            MdtContextParser.State.GET_TOKEN: self.get_next_token,
+            MdtContextParser.State.EVAL_TOKEN: self.evaluate_token,
+            MdtContextParser.State.ADD_ENTITIES: self.add_entities,
+            MdtContextParser.State.ADD_DIRECTIONS: self.add_directions,
+            MdtContextParser.State.ADD_ENTITIES_AND_DIRECTIONS: self.parse_entities_and_dir,
+        }
+
+    def _precondition_lines(self, line: str):
+        # Convert everything to lowercase for matching purposes
+        line = line.lower()
+
+        # Helps with tokenizing entities and directions
+        # 'two hops are southeast' is logically equivalent to 'two hops is southeast', just bad english
+        line = line.replace("are", "is")
+
+        # TODO: Determine if this one is safe to tokenize
+        line = line.replace(
+            "and the limit of your vision", ", the limit of your vision"
+        )
+
+        # Commas are the main tokens that can be used to identify room elements
+        lines = line.split(", ")
+
+        # Remove room entries that we don't care about (along with their directions!)
+        lines = filter_exits(lines)
+
+        return lines
+
+    def read(self) -> dict[tuple[tuple[int, str]], RoomInfo]:
+        self.state = MdtContextParser.State.GET_TOKEN
+        while self.state != MdtContextParser.State.DONE:
+            self.state_executor[self.state]()
+
+        if len(self.entity_stack) > 0 and len(self.direction_stack) > 0:
+            self.push_room()
+
+        return self.mdt_rooms
+
+    def get_next_token(self):
+        if self.token_idx < self.num_tokens:
+            self.token = self.tokens[self.token_idx].rstrip().lstrip()
+            self.state = MdtContextParser.State.EVAL_TOKEN
+        else:
+            self.state = MdtContextParser.State.DONE
+
+    def evaluate_token(self):
+        fragments = self.token.split(" is ")
+        directions = None if len(fragments) < 2 else fragments[1]
+
+        # 'neelie awkside and a sailor is one southwest'
+        if directions is not None:
+            self.entities = fragments[0]
+            self.directions = fragments[1]
+            self.state = MdtContextParser.State.ADD_ENTITIES_AND_DIRECTIONS
+        else:
+            words = self.word_tokens[self.token_idx]
+            if len(words) < 2:
+                # Has to be a named NPC or Player
+                self.entities = fragments[0]
+                self.state = MdtContextParser.State.ADD_ENTITIES
+                return
+
+            if words[0] in NUMBER_MAP and words[1] in DIRECTION_MAP:
+                self.directions = fragments[0]
+                self.state = MdtContextParser.State.ADD_DIRECTIONS
+                return
+            else:
+                # Has to be an entity
+                self.entities = fragments[0]
+                self.state = MdtContextParser.State.ADD_ENTITIES
+                return
+
+    def add_entities(self):
+        # If we have directions in the stack, we have the start of a new room definition..
+        if len(self.direction_stack) > 0:
+            self.push_room()
+
+        push_entities(self.entities, self.entity_stack)
+        self.state = MdtContextParser.State.GET_TOKEN
+        self.token_idx += 1
+
+    def add_directions(self):
+        push_directions(self.directions, self.direction_stack)
+        self.state = MdtContextParser.State.GET_TOKEN
+        self.token_idx += 1
+
+    def parse_entities_and_dir(self):
+        # If we have directions in the stack, we have the start of a new room definition..
+        if len(self.direction_stack) > 0:
+            self.push_room()
+
+        push_entities(self.entities, self.entity_stack)
+        push_directions(self.directions, self.direction_stack)
+        self.token_idx += 1
+        self.state = MdtContextParser.State.GET_TOKEN
+
+    def push_room(self):
+        room = RoomInfo()
+        room.entities.extend(self.entity_stack)
+        self.mdt_rooms[tuple(self.direction_stack)] = room
+        self.direction_stack.clear()
+        self.entity_stack.clear()
---
+++ src/mdt/watcher.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+
+import logging
+import curses
+import time
+import os
+import re
+
+from definitions import (
+    RoomInfo,
+)
+from config import (
+    USER_MATCHES,
+    MDT_PARSE_DIR,
+    MIN_ROOM_VALUE,
+    DEFAULT_NPC_VALUE,
+    CURSES_COLOR_PAIR_MAP,
+    MdtColors,
+)
+from tintin import transform_tintin_array, is_tintin_array
+from mdt.parser import MdtContextParser
+
+logging.basicConfig(
+    filename="crash.mdt.log",
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(name)s %(message)s",
+)
+_LOGGER = logging.getLogger(__name__)
+
+
+def write_rooms_to_console(room_data: dict[tuple[int, str], RoomInfo]):
+    """Writes the rooms to the terminal.
+
+    Args:
+        room_data (dict[tuple[int, str], RoomInfo]): The room data to write
+    """
+    stdscr.clear()
+    y = 0
+    x = 0
+
+    if len(room_data) == 0:
+        stdscr.addstr(y, x, "Nothing seen!")
+        stdscr.refresh()
+        return
+
+    def calc_longest_room_dir_length(room_data: dict):
+        longest_dir = -1
+        for room_dir in room_data.keys():
+            dir_str = ""
+            # 1 s, 2 sw
+            for dir in room_dir:
+                dir_str += f"{dir[0]} {dir[1]},"
+
+            longest_dir = max(longest_dir, len(dir_str))
+
+        return longest_dir
+
+    max_y, max_x = stdscr.getmaxyx()
+
+    # Ensure that all the directions line up in the output
+    score_pos_offset = calc_longest_room_dir_length(room_data)
+
+    stdscr.clear()
+    y = 0
+    for room_dir, room_info in sorted(
+        room_data.items(), key=lambda x: x[1].score, reverse=True
+    ):
+        if y >= max_y:
+            break
+
+        x = 0
+        for i, dir in enumerate(room_dir):
+            dir_str = (
+                f"{dir[0]} {dir[1]}"
+                if i == len(room_dir) - 1
+                else f"{dir[0]} {dir[1]},"
+            )
+            if x + len(dir_str) < max_x:
+                stdscr.addstr(y, x, dir_str)
+                x += len(dir_str)
+            else:
+                break
+
+        room_score_str = f"[{room_info.score}] "
+        x = score_pos_offset
+
+        if x + len(room_score_str) < max_x:
+            # Add the [score]
+            stdscr.addstr(
+                y, x, room_score_str, curses.color_pair(MdtColors.CYAN_BLACK.value)
+            )
+            x += len(room_score_str)
+        else:
+            break
+
+        for i, entity in enumerate(room_info.entities):
+            entity_str = (
+                f"{entity.description}"
+                if i == len(room_info.entities) - 1
+                else f"{entity.description}, "
+            )
+
+            if x + len(entity_str) < max_x:
+                stdscr.addstr(
+                    y, x, entity_str, curses.color_pair(entity.curse_color_code)
+                )
+                x += len(entity_str)
+            else:
+                break
+
+        y += 1
+
+    stdscr.refresh()
+
+
+def apply_match_configs(
+    room_data: dict[tuple[tuple[int, str]], RoomInfo]
+) -> dict[tuple[tuple[int, str]], RoomInfo]:
+    """Applys the user config regex matches to rooms parsed from the MDT information.
+    Scores the entities in the associated rooms.
+    Associates colors with each entity in the room.
+
+    Args:
+        room_data (dict[tuple[int, str], RoomInfo]): The parsed rooms
+
+    Returns:
+        dict[tuple[int, str], RoomInfo]: The filtered, colored and scored rooms
+    """
+    for room_info in room_data.values():
+        room_score = 0
+
+        for entity in room_info.entities:
+            # There has got to be a more efficient way of doing this
+            entity_score = DEFAULT_NPC_VALUE
+
+            for match in USER_MATCHES:
+                # If pattern is a string, just search in string
+                if (
+                    isinstance(match.pattern, str)
+                    and match.pattern in entity.description
+                ) or (
+                    isinstance(match.pattern, re.Pattern)
+                    and match.pattern.match(entity.description)
+                ):
+                    # Score = score per entity * entity count
+                    entity.score = match.score
+                    entity_score = match.score * entity.count
+                    entity.curse_color_code = match.curses_color_code
+                    break
+
+            room_score += entity_score
+
+        room_info.score = room_score
+
+    # Filter Rooms that are below min score
+    filtered_rooms = {k: v for k, v in room_data.items() if v.score >= MIN_ROOM_VALUE}
+
+    # TODO: Sort rooms right above this line to optimize how many NPC lists we need to sort
+    for info in filtered_rooms.values():
+        info.entities.sort(key=lambda x: x.score, reverse=True)
+
+    return filtered_rooms
+
+
+def to_mdt_rooms(lines: list[str]) -> list[str]:
+    # The output from tt++ is always a single line
+    try:
+        mdt_line = lines[0]
+        if is_tintin_array(mdt_line):
+            # Parses the last element of the tintin array output as the
+            # manual map door text line
+            mdt_line = transform_tintin_array(mdt_line)
+
+        reader = MdtContextParser(mdt_line)
+        mdt_data = reader.read(mdt_line)
+        mdt_data = apply_match_configs(mdt_data)
+        write_rooms_to_console(mdt_data)
+    except Exception as e:
+        _LOGGER.error(e)
+        write_rooms_to_console({})
+
+
+def watch_files(filename: str):
+    last_update_time = os.stat(filename).st_mtime
+
+    with open(filename, "r") as f:
+        while True:
+            new_time = os.stat(filename).st_mtime
+
+            # If the files modified time has changed, run the parser
+            if new_time != last_update_time:
+                last_update_time = new_time
+                to_mdt_rooms(f.readlines())
+                f.seek(0)
+
+            time.sleep(0.2)
+
+
+if __name__ == "__main__":
+    try:
+        stdscr = curses.initscr()
+        curses.start_color()
+        curses.use_default_colors()
+        curses.curs_set(0)
+
+        # Initialize MDT color pairs after we've initialized the curse screen
+        for mdt_color, curses_color_pair in CURSES_COLOR_PAIR_MAP.items():
+            curses.init_pair(
+                mdt_color.value, curses_color_pair[0], curses_color_pair[1]
+            )
+
+        mdt_log_path = os.path.abspath(
+            os.path.join(MDT_PARSE_DIR, "../../logs/mapdoortext.log")
+        )
+
+        watch_files(mdt_log_path)
+    except Exception as e:
+        _LOGGER.error(e)
+    finally:
+        # Cleanup
+        curses.nocbreak()
+        stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
