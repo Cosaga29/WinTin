0c56cdb3703db63e6d9acb43a74e709dfabc3351
Alex <maedmun2@gmail.com>
2025-01-27 11:12:47 -0500

    most up to date mdt

--- src/mdt/patterns.py
+++ src/mdt/patterns.py
@@ -3,4 +3,4 @@ import re
 TINTIN_ARRAY_BETWEEN = re.compile(r"}{\d+}{")
 TINTIN_ARRAY_BEGIN = re.compile(r"\d+}{")
 MDT_COMMAND_QUEUE = re.compile(r"Queued command: map door text ")
-PLAYER_COLOR = re.compile(r"\x1b.*\x1b\[\d+m(.*)\x1b.*\x1b")
+PLAYER_COLOR = re.compile(r"\x1b\[.*m(.*)\x1b.*\x1b")
