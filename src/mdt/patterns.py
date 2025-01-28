7a4fc0e403cdb4a44ffef6c916631e03a8e1d5ff
Alex <maedmun2@gmail.com>
2025-01-28 01:18:52 -0500

    merged from master

--- src/mdt/patterns.py
+++ src/mdt/patterns.py
@@ -13,6 +13,7 @@ PLAYER_COLOR_2 = re.compile(r"\x1b\[[0-9]+;[0-9]+m(.*)\x1b.*\x1b")
 QUEUED_COMMAND_REGEX = re.compile(r"}{[0-9]+}{Queued command: .*}{[0-9]+}{")
 HP_MONITOR_REGEX = re.compile(r"}{Hp: .*}{[0-9]+}{")
 CURSOR_REGEX = re.compile(r"}> {[0-9]+}{")
+ALT_CURSOR_REGEX = re.compile(r"{[0-9]+}{> }{[0-9]+}{")
 COMBINED_REGEX = re.compile(r"{[0-9]+}{Queued command: .*}{[0-9]+}{> }{[0-9]+}")
 
-CLIENT_ASYNC_TOKENS = [QUEUED_COMMAND_REGEX, HP_MONITOR_REGEX, CURSOR_REGEX, COMBINED_REGEX]
+CLIENT_ASYNC_TOKENS = [QUEUED_COMMAND_REGEX, HP_MONITOR_REGEX, CURSOR_REGEX, ALT_CURSOR_REGEX, COMBINED_REGEX]
